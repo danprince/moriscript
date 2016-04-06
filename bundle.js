2ffe2e88700edb47a44bed1fe225a0819219bb4b
danprince <danielprince@hotmail.co.uk>
2016-04-06 15:32:29 +0100

    updates bundle to correct version

--- bundle.js
+++ bundle.js
@@ -33831,12 +33831,14 @@ module.exports = function(babel) {
   return {
     visitor: {
       ObjectExpression(path) {
-        if(path.node.isClean) return;
-        path.node.isClean = true;
+        const props = [];
 
-        const props = path.node.properties.reduce(function(props, prop) {
-          return props.concat([t.stringLiteral(prop.key.name), prop.value]);
-        }, []);
+        path.node.properties.forEach(prop => {
+          props.push(
+            t.stringLiteral(prop.key.name),
+            prop.value
+          );
+        });
 
         path.replaceWith(
           t.callExpression(
@@ -33846,13 +33848,10 @@ module.exports = function(babel) {
         );
       },
       ArrayExpression(path) {
-        if(path.node.isClean) return;
-        path.node.isClean = true;
-
         path.replaceWith(
           t.callExpression(
-            moriMethod('toClj'),
-            [path.node]
+            moriMethod('vector'),
+            path.node.elements
           )
         );
       },
@@ -33861,29 +33860,27 @@ module.exports = function(babel) {
         const rhs = path.node.right;
 
         if(t.isMemberExpression(lhs)) {
-          var prop = lhs.property;
           if(t.isIdentifier(lhs.property)) {
-            prop = t.stringLiteral(lhs.property.name);
+            lhs.property = t.stringLiteral(lhs.property.name);
           }
 
           path.replaceWith(
             t.callExpression(
               moriMethod('assoc'),
-              [lhs.object, prop, rhs]
+              [lhs.object, lhs.property, rhs]
             )
           );
         }
       },
       MemberExpression(path) {
-        // if the parent is an assignment expression, handle it elsewhere
-        if(t.isAssignmentExpression(path.parent)) return;
         if(path.node.isClean) return;
-        if(path.node.object.name == 'console') return;
-
-        var prop = path.node.property;
+        if(t.isAssignmentExpression(path.parent)) return;
+        if(path.node.object.name == 'console' ||
+           path.node.object.name == 'window'  ||
+           path.node.object.name == 'global') return;
 
         if(t.isIdentifier(path.node.property)) {
-          prop = t.stringLiteral(prop.name);
+          path.node.property = t.stringLiteral(path.node.property.name);
         }
 
         path.replaceWith(
@@ -33895,6 +33892,7 @@ module.exports = function(babel) {
       },
       CallExpression(path) {
         const callee = path.node.callee;
+
         if(t.isMemberExpression(callee)) {
           if(callee.object.name == 'console' && callee.property.name == 'log') {
             path.node.arguments = path.node.arguments.map(function(expr) {
